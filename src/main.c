e5af9f2401c3e0f5601529f658332e3751e9705b
Lab ESPL Stud06 lab_espl_stud06 <lab_espl_stud06@tueircs-ldb16.int.rcs.ei.tum.de>
2019-11-05 18:33:23 +0100

    c Programm

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
-void main()
+#include "espl_lib.h"
+
+int main()
 {
-       printf("\n Hello Espl\n");
-       printf("\n new line\n");
+	int n,i;
+	char answer;
+	
+	printf("\n Hello Espl\n");
+	printf("\n new line\n");
+	
+	i=1;
+	while (i!=0) {
+	      
+		printf("\n Enter a number: ");
+		scanf("%d", &n);
+		getchar();
+	
+		printf("the numb in word is: %s" , num_to_words(n));
+	
+		    
+		printf("\nWould you like to continue? Enter Y or N: \n");
+		scanf("%c", &answer);
+		printf("\n answer is %s", &answer);
+		if (answer == 'Y'){
+			i=1;}
+		
+
+		else {
+			i=0;}
+	}
+	
 }
